0734b9a16054154c62befd22fff4e6ceaa972175
GrebenyukArtemkin <DGrebenyuk2001@yandex.ru>
2024-06-19 14:33:38 +0300

    1

--- GrebenyukArtemkin/GrebenyukArtemkin.cpp
+++ GrebenyukArtemkin/GrebenyukArtemkin.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "GrebenyukArtemkin!\n";
+    std::cout << "blablablaGrebenyukArtemkin!\n";
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
